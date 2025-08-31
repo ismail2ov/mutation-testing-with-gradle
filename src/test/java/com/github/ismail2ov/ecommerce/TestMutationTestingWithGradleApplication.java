af3e7448774168c7aa867df3e558687e517ca9e8
Ismail Ahmedov <ismail2ov@gmail.com>
2025-08-31 19:32:12 +0200

    Apply spotless

--- src/test/java/com/github/ismail2ov/ecommerce/TestMutationTestingWithGradleApplication.java
+++ src/test/java/com/github/ismail2ov/ecommerce/TestMutationTestingWithGradleApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 
 public class TestMutationTestingWithGradleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(MutationTestingWithGradleApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.from(MutationTestingWithGradleApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
 
 }
